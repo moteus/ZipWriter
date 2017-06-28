482b61e2a105262e2b76d3728759918a15663b82
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-06-28 09:50:30 +0300

    Update copyright date.

--- lua/ZipWriter/module.lua
+++ lua/ZipWriter/module.lua
@@ -1,6 +1,6 @@
 return {
   _NAME      = "ZipWriter";
   _VERSION   = "0.1.6-dev";
-  _COPYRIGHT = "Copyright (C) 2013-2016 Alexey Melnichuk";
+  _COPYRIGHT = "Copyright (C) 2013-2017 Alexey Melnichuk";
   _LICENSE   = "MIT";
 }
